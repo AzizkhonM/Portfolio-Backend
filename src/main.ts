4357c076ae40c7aa298a7076051c96593260abdb
Azizxon <muzaffarovazizkhon@gmail.com>
2023-07-09 17:11:24 +0500

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -95,6 +95,18 @@ const start = async () => {
 
     const PORT = process.env.PORT || 7000;
 
+    app.enableCors({
+      allowedHeaders: ['content-type'],
+      origin: [
+        'http://localhost:5174',
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        '*',
+      ],
+      credentials: true,
+    });
+
     app.setGlobalPrefix('api');
 
     const config = new DocumentBuilder()
